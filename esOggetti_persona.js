23230420ad9c747c85add437dca8747c637be423
francescodella007 <155648739+francescodella007@users.noreply.github.com>
2025-05-07 11:13:33 +0200

    Update esOggetti_persona.js

--- esOggetti_persona.js
+++ esOggetti_persona.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>esOggetti_calendario</title>
+    <title>esOggetti_persona</title>
 </head>
 <body>
     <button onclick="dataScadenza()">Imposta data di scadenza</button>
