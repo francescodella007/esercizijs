331661d8ef798c3545ec0581e931cee79165d971
francescodella007 <155648739+francescodella007@users.noreply.github.com>
2025-05-07 11:11:46 +0200

    Update esOggetti_libro.js

--- esOggetti_libro.js
+++ esOggetti_libro.js
@@ -1,11 +1,27 @@
-const libro = {
-    titolo: "Il nome della rosa",
-    autore: "Umberto Eco",
-    anno: 1980,
-    
-    descrizione: function() {
-      return "Dati del libro\nTitolo:"+this.titolo+"\nAutore: "+this.autore+"\nAnno: "+this.anno;
-    }
-  };
-  
-console.log(libro.descrizione());
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>esOggetti_calendario</title>
+</head>
+<body>
+    <button onclick="dataScadenza()">Imposta data di scadenza</button>
+
+    <div id="output"></div>
+
+    <script>
+        const libro = {
+            titolo: "Il nome della rosa",
+            autore: "Umberto Eco",
+            anno: 1980,
+            
+            descrizione: function() {
+              return "Dati del libro\nTitolo:"+this.titolo+"\nAutore: "+this.autore+"\nAnno: "+this.anno;
+            }
+          };
+          
+        console.log(libro.descrizione());
+    </script>
+</body>
+</html>
