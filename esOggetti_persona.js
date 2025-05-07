6beb30aae2153e4fef709ec85fab3bdb85f7d951
francescodella007 <155648739+francescodella007@users.noreply.github.com>
2025-05-07 11:13:16 +0200

    Update esOggetti_persona.js

--- esOggetti_persona.js
+++ esOggetti_persona.js
@@ -1,14 +1,30 @@
-const persona = {
-    nome: "Francesco",
-    cognome: "Dell'Aquila",
-    annoNascita: 2007,
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>esOggetti_calendario</title>
+</head>
+<body>
+    <button onclick="dataScadenza()">Imposta data di scadenza</button>
 
-    calcolaEta: function() {
-      const annoCorrente = 2025;
-      return annoCorrente - this.annoNascita;
-    },
-    nomeCompleto: function() {
-      return `${this.nome} ${this.cognome}`;
-    }
-  };
-console.log("Nome e cognome: ",persona.nomeCompleto(),"\nEtà: ",persona.calcolaEta());
+    <div id="output"></div>
+
+    <script>
+        const persona = {
+            nome: "Francesco",
+            cognome: "Dell'Aquila",
+            annoNascita: 2007,
+        
+            calcolaEta: function() {
+              const annoCorrente = 2025;
+              return annoCorrente - this.annoNascita;
+            },
+            nomeCompleto: function() {
+              return `${this.nome} ${this.cognome}`;
+            }
+          };
+        console.log("Nome e cognome: ",persona.nomeCompleto(),"\nEtà: ",persona.calcolaEta());
+    </script>
+</body>
+</html>
